9e212eebac5e17c657032f817778856485daa301
Aaron <aaronthakoordeen@gmail.com>
2024-03-22 15:59:17 -0400

    FIX general file structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
-import { TransitionProvider } from '@/components/transitionProvider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransitionProvider>{children}</TransitionProvider>
+      <div className="container">
+        <Navbar />
+        {children}
+        <Footer />
+        </div>
         </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,53 +1,10 @@
-"use client"
-
-import { motion } from "framer-motion"
 import React from "react";
 import styles from "./homepage.module.css"
 
-const Home = () => {
+export default function Home() {
   return (
-    <section
-      className={styles.intro}
-    >
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity:0, scale:0 }}
-            animate={{ opacity:1, scale:1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
+    <div className={styles.container}>
 
-          </motion.div> 
-          <motion.span
-            className="absolute bottom-0 right-0 text-4x1"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          ></motion.span>
-        </div>
-      </div>
-
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold"> Hello! Welcome to </span> 
-        <span className="underline"> FINET.</span>
-
-        
-      </motion.h1>
-      
-    </section>
+    </div>
   )
-};
-
-export default Home;
+}
