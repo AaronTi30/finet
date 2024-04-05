0875ffca3c173e9b00f606c8f88eb3268c500824
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-04-05 19:06:19 -0400

    2024-04-05: finished initial code for 'about file' need to edit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,21 @@ import * as classes from "./about.module.css";
 
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
-    )
-}
+        <div className={classes.box}>
+            <div className={classes["base-layer"]}>
+                <div className={classes["overlap-group"]}>
+                    <div className={classes.group}>
+                        <div className={classes["text-wrapper"]}>About Us</div>
+                    </div>
+                    <p className={classes.div}>Making Connections For Students Easier Than Ever!</p>
+                </div>
+                <div className={classes["text-wrapper-2"]}>Why We Started FINET</div>
+                <p className={classes["description"]}>
+                {/* description goes here */}
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default AboutPage
