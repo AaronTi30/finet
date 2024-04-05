e6e17cebd5fe4d9003d3515373c8096fd4287348
joseateodoro <joseateodoro.jt@gmail.com>
2024-04-04 23:02:34 -0400

    2024-04-04: updated homepage.module.css + page.jsx 'homepage'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./homepage.module.css"
+import * as classes from ".homepage.module.css"
 import SectionDivider from "@/components/section-divider/section-divider";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <SectionDivider />
+      {/* <SectionDivider /> */}
+      <div className={classes.box}>
+        <div className={classes["finet-logo-btn"]}>
+          
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import Links from "./links/Links"
 import styles from "./navbar.module.css"
+import Image from "./images/finet_logo_white.png"
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
-            <img className={styles.logo} alt="FINET" src="../images/finet_logo_white.png"/>
+            <img className={styles.logo} src={Image} alt="FINET" />
             <div>
                 <Links/>
             </div>
