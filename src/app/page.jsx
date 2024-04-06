b1b6a4855dc96104dab7f8411d8767edeaa64607
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-04-05 22:48:16 -0400

    2024-04-05: fixed classes clause in 'homepage.jsx'. need to fix module.css files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,14 +8,24 @@ export default function Home() {
     <div className={styles.container}>
       {/* <SectionDivider /> */}
       <div className={classes.box}>
+        {/* HOME PAGE */}
         <div className={classes["finet-logo-btn"]}>
-          <div className="overlap-group">
-            <img className="finet-logo-white-txt" alt="LOGO" src="../components/images/finet_logo_white_txt.png"/>
-            <div className="rectangle" />
-            <div className="div" />
-            <div className="text-wrapper">Join The Team</div>
-            <div className="text-wrapper-2">View Our Work</div>
+          <div className={classes["overlap-group"]}>
+            <img className={classes["finet-logo-white-txt"]} alt="LOGO" src="../components/images/finet_logo_white_txt.png"/>
+            <div className={classes["rectangle"]}/>
+            <div className={classes["div"]}/>
+            <div className={classes["text-wrapper"]}>Join The Team</div>
+            <div className={classes["text-wrapper-2"]}>View Our Work</div>
           </div>
+        </div>
+        {/* HOME PAGE MESSAGE */}
+
+        {/* HOME PAGE VIDEO */}
+
+        {/* HOME PAGE TESTIMONIALS */}
+        <div className={classes["test-title"]}>
+          <div className="text-wrapper">TESTIMONIALS</div>
+          <div className="test-line"/>
         </div>
       </div>
     </div>
