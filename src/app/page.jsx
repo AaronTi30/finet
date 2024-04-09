ac3818e4f3cdbd3ab15f58d1fad541bc9b8094d3
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-04-09 13:29:25 -0400

    2024-04-09: updated 'homepage.jsx', fixed testimonial card positioning, need to fix videoview, profile images, and logo positioning

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./homepage.module.css"
 import * as classes from "./homepage.module.css"
+import YoutubeEmbed from "@/components/javascript-files/youtubeembed";
 import SectionDivider from "@/components/section-divider/section-divider";
 
 export default function Home() {
@@ -23,6 +24,9 @@ export default function Home() {
       {/* HOME PAGE MESSAGE */}
 
       {/* HOME PAGE VIDEO */}
+      {/* <div className={classes.home}>
+        <YoutubeEmbed embedId=""/>
+      </div> */}
 
       {/* HOME PAGE TESTIMONIALS */}
       <div className={classes.home}>
@@ -31,12 +35,57 @@ export default function Home() {
             <div className={classes["test-text-wrapper"]}>TESTIMONIALS</div>
         </div>
       </div>
+      {/* Sowmya Desaraju */}
       <div className={classes.testcard}>
         <div className={classes["card-1"]}>
           <div className={classes["overlap-group"]}> 
             <div className={classes["speaker"]}>-FINET Speaker</div>
             <div className={classes["name"]}>-Sowmya Desaraju</div>
             <img className={classes["image"]} alt="SDP" src="../components/images/Sowmya Desaraju (BNP Paribas).jpeg"/>
+            <p className={classes.description}>
+              {/* description goes here */}
+            </p>
+          </div>
+        </div>
+        {/* William Meyer */}
+        <div className={classes["card-2"]}>
+          <div className={classes["overlap-group"]}> 
+            <div className={classes["speaker"]}>-FINET Speaker</div>
+            <div className={classes["name"]}>-William Meyer</div>
+            <img className={classes["image"]} alt="SDP" src="../components/images/William Meyer (Blue Origin).png"/>
+            <p className={classes.description}>
+              {/* description goes here */}
+            </p>
+          </div>
+        </div>
+        {/* Helen Geris */}
+        <div className={classes["card-3"]}>
+          <div className={classes["overlap-group"]}> 
+            <div className={classes["speaker"]}>-FINET Speaker</div>
+            <div className={classes["name"]}>-Helen Geris</div>
+            <img className={classes["image"]} alt="SDP" src="../components/images/Helen Geris (LimeStone Therapies).jpg"/>
+            <p className={classes.description}>
+              {/* description goes here */}
+            </p>
+          </div>
+        </div>
+        {/* Charlotte Grandy */}
+        <div className={classes["card-4"]}>
+          <div className={classes["overlap-group"]}> 
+            <div className={classes["speaker"]}>-FINET Speaker</div>
+            <div className={classes["name"]}>-Charlotte Grandy</div>
+            <img className={classes["image"]} alt="SDP" src="../components/images/Charlotte Grandy (Hudson Bay).jpg"/>
+            <p className={classes.description}>
+              {/* description goes here */}
+            </p>
+          </div>
+        </div>
+        {/* Umar Makhdoom */}
+        <div className={classes["card-5"]}>
+          <div className={classes["overlap-group"]}> 
+            <div className={classes["speaker"]}>-FINET Speaker</div>
+            <div className={classes["name"]}>-Umar Makhdoom</div>
+            <img className={classes["image"]} alt="SDP" src="../components/images/Umar Makhdoom (Xero).jpg"/>
             <p className={classes.description}>
               {/* description goes here */}
             </p>
