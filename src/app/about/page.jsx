51e98291e6e7cfc10e1009f414793bb1c2464d36
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-04-09 15:55:28 -0400

    2024-04-09: updated 'aboutpage.jsx', working on founders/team page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import * as classes from "./about.module.css";
 
 const AboutPage = () => {
     return (
-        <div className={classes.box}>
+        <div className={classes.about}>
             <div className={classes["base-layer"]}>
                 <div className={classes["overlap-group"]}>
                     <div className={classes.group}>
@@ -16,7 +16,23 @@ const AboutPage = () => {
                 {/* description goes here */}
                 </p>
             </div>
+            <div className={classes.founders}>
+                <div className={classes["about-us-own"]}>
+                    <div className={classes.meet-group}>
+                        <div className={classes["meet-text-wrapper"]}>MEET</div>
+                        <div className={classes.found-text-wrapper}>THE FOUNDERS</div>
+                    </div>
+                    {/* Yash */}
+                    <img className={classes.meet-group} src="" alt="" />
+                    <div className={classes["name-text-wrapper"]}>John Jones</div>
+                    <p className={classes["name-text-wrapper"]}>Founder - Student- Product Manager</p>
+                    <p className={classes["note-text-warpper"]}>
+                        {/* description goes here */}
+                    </p>
+                </div>
+            </div>
         </div>
+
     );
 };
 
